c4bb2db13991dd5844615712759303613dd842d6
Mathieu Morainville <mathieumorainville@hotmail.com>
2017-08-18 22:49:37 +0200

    feat: add Bulma and a template

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,102 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        perudo-calculator
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+  <section class="hero is-info is-fullheight">
+    <!-- Hero header: will stick at the top -->
+    <div class="hero-head">
+      <header class="nav">
+        <div class="container">
+          <div class="nav-left">
+            <a class="nav-item">
+              Perudo Calculator
+            </a>
+          </div>
+          <span class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+          <div class="nav-right nav-menu">
+            <a class="nav-item is-active">
+              Home
+            </a>
+            <a class="nav-item">
+              Examples
+            </a>
+            <a class="nav-item">
+              Documentation
+            </a>
+            <span class="nav-item">
+              <a class="button is-success is-inverted">
+                <span class="icon">
+                  <i class="fa fa-github"></i>
+                </span>
+                <span>Download</span>
+              </a>
+            </span>
+          </div>
+        </div>
+      </header>
+    </div>
+
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">
+          Parmis <input class="c-input-number c-input-number--title" type="number" placeholder="n"> dés, il y a
+        </h1>
+        <h2 class="subtitle">
+          (en règles normales)
+        </h2>
+
+        <div class="level">
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading">
+                exactement <input class="c-input-number c-input-number--heading" type="number" placeholder="k">
+                fois le nombre <input class="c-input-number c-input-number--heading" type="number" placeholder="x">
+                ou le paco
+              </p>
+              <p class="title">3,456</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading">au moins k fois le nombre l ou le paco</p>
+              <p class="title">123</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="level">
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading">exactement k fois le paco</p>
+              <p class="title">456K</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered">
+            <div>
+              <p class="heading">au moins k fois le paco</p>
+              <p class="title">789</p>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+
+    <!-- Hero footer: will stick at the bottom -->
+    <div class="hero-foot">
+      <nav class="tabs is-boxed is-fullwidth">
+        <div class="container">
+          <ul>
+            <li class="is-active"><a>Overview</a></li>
+            <li><a>Modifiers</a></li>
+            <li><a>Grid</a></li>
+            <li><a>Elements</a></li>
+            <li><a>Components</a></li>
+            <li><a>Layout</a></li>
+          </ul>
+        </div>
+      </nav>
     </div>
   </section>
 </template>
@@ -26,34 +111,28 @@ export default {
 }
 </script>
 
-<style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
+<style lang="scss">
+.c-input-number {
+  background: transparentize(white, 0.9);
+  border: none;
+  text-align: left;
+  padding-left: 16px;
+  color: white;
+  font-weight: 600;
+
+  &::placeholder {
+    color: white;
+  }
+
+  &--title {
+    line-height: 1.125;
+    font-size: 2rem;
+    width: 64px;
+  }
+
+  &--heading {
+    font-size: 1rem;
+    width: 48px;
+  }
 }
 </style>
