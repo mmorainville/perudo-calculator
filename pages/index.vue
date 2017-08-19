93b97cd31deadd3ad6cffcf6829b0a595bd0722a
Mathieu Morainville <mathieumorainville@hotmail.com>
2017-08-19 04:30:28 +0200

    feat: add conditional probabilities

--- pages/index.vue
+++ pages/index.vue
@@ -33,7 +33,7 @@
     <div class="hero-body">
       <div class="container has-text-centered">
         <h1 class="title">
-          Parmis <input v-model.number="n" class="c-input-number c-input-number--title" type="number" placeholder="n" min="0" step="1"> dés, il y a
+          Parmi <input v-model.number="n" class="c-input-number c-input-number--title" type="number" placeholder="n" :min="k" step="1"> dés, il y a
         </h1>
         <h2 class="subtitle">
           (en règles normales)
@@ -74,23 +74,45 @@
         </div>
 
         <h1 class="title">
-          Parmis mes <input class="c-input-number c-input-number--title" type="number" placeholder="m"> dés, j'ai
+          Parmis mes <input v-model.number="m" class="c-input-number c-input-number--title" type="number" placeholder="m" min="1" :max="n > 5 ? 5 : n">
+          dés, j'ai exactement <input v-model.number="j" class="c-input-number c-input-number--title" type="number" placeholder="j" min="0" :max="m">
+          fois le nombre <input v-model.number="l" class="c-input-number c-input-number--title" type="number" placeholder="x" min="2" max="6">
+          ou le paco
         </h1>
+        <h2 class="subtitle">
+          alors, parmi {{ n || 'n' }} dés, il y a
+        </h2>
 
         <div class="tile is-ancestor c-values">
           <div class="tile is-parent">
             <div class="tile is-child box is-shadowless has-text-white c-values__block c-values__block--primary">
               <p class="heading">
-                exactement j fois le nombre l ou le paco
+                exactement <input v-model.number="k" class="c-input-number c-input-number--heading" type="number" placeholder="k" min="0" :max="n" step="1">
+                fois le nombre <input v-model.number="l" class="c-input-number c-input-number--heading" type="number" placeholder="x" min="2" max="6">
+                ou le paco
               </p>
-              <p class="title">3,456</p>
+              <p class="title">{{ Pankl_mjl | percentage }}</p>
             </div>
           </div>
 
           <div class="tile is-parent">
             <div class="tile is-child box is-shadowless has-text-white c-values__block c-values__block--primary">
-              <p class="heading">exactement j fois le paco</p>
-              <p class="title">123</p>
+              <p class="heading">au moins {{ k }} fois le nombre {{ l || 'x' }} ou le paco</p>
+              <p class="title">{{ PAnkl_mjl | percentage }}</p>
+            </div>
+          </div>
+
+          <div class="tile is-parent">
+            <div class="tile is-child box is-shadowless has-text-white c-values__block c-values__block--primary">
+              <p class="heading">exactement k fois le paco</p>
+              <p class="title">{{ Pankpaco_mjpaco | percentage }}</p>
+            </div>
+          </div>
+
+          <div class="tile is-parent">
+            <div class="tile is-child box is-shadowless has-text-white c-values__block c-values__block--primary">
+              <p class="heading">au moins k fois le paco</p>
+              <p class="title">{{ PAnkpaco_mjpaco | percentage }}</p>
             </div>
           </div>
         </div>
@@ -123,39 +145,58 @@
     computed: {
       // Parmis n dés, il y a exactement k fois le nombre l ou le paco
       Pankl () {
-        try {
-          return math.eval(`${math.combinations(this.n, this.k)} * ((2 ^ (${this.n} - ${this.k})) / (3 ^ ${this.n}))`)
-        } catch (error) { console.warn(error) }
+        return this.getExactly(this.n, this.k)
       },
       // Parmis n dés, il y a au moins k fois le nombre l ou le paco
       PAnkl () {
-        try {
-          let result = null
-          for (let i = 0; i < this.n - this.k + 1; i++) {
-            result += math.eval(`${math.combinations(this.n, i + this.k)} * ((2 ^ (${this.n} - ${i + this.k})) / (3 ^ ${this.n}))`)
-          }
-          return result
-        } catch (error) { console.warn(error) }
+        return this.getAtLeast(this.n, this.k)
       },
       // Parmis n dés, il y a exactement k fois le paco
       Pankpaco () {
-        try {
-          return math.eval(`${math.combinations(this.n, this.k)} * ((5 ^ (${this.n} - ${this.k})) / (6 ^ ${this.n}))`)
-        } catch (error) { console.warn(error) }
+        return this.getExactly(this.n, this.k, true)
+      },
+      // Parmis n dés, il y a au moins k fois le paco
+      PAnkpaco () {
+        return this.getAtLeast(this.n, this.k, true)
+      },
+      Pankl_mjl () {
+        return this.getExactly(this.n - this.m, this.k - this.j)
+      },
+      // Parmis n dés, il y a au moins k fois le nombre l ou le paco
+      PAnkl_mjl () {
+        return this.getAtLeast(this.n - this.m, this.k - this.j)
       },
       // Parmis n dés, il y a exactement k fois le paco
-      PAnkpaco () {
+      Pankpaco_mjpaco () {
+        return this.getExactly(this.n - this.m, this.k - this.j, true)
+      },
+      // Parmis n dés, il y a au moins k fois le paco
+      PAnkpaco_mjpaco () {
+        return this.getAtLeast(this.n - this.m, this.k - this.j, true)
+      }
+    },
+    methods: {
+      getExactly (n, k, isPalifico) {
+        let numerator = isPalifico ? 5 : 2
+        let denominator = isPalifico ? 6 : 3
+        try {
+          return math.eval(`${math.combinations(n, k)} * ((${numerator} ^ (${n} - ${k})) / (${denominator} ^ ${n}))`)
+        } catch (error) { console.warn(error) }
+      },
+      getAtLeast (n, k, isPalifico) {
+        let numerator = isPalifico ? 5 : 2
+        let denominator = isPalifico ? 6 : 3
         try {
           let result = null
-          for (let i = 0; i < this.n - this.k + 1; i++) {
-            result += math.eval(`${math.combinations(this.n, i + this.k)} * ((5 ^ (${this.n} - ${i + this.k})) / (6 ^ ${this.n}))`)
+          for (let i = 0; i < n - k + 1; i++) {
+            result += math.eval(`${math.combinations(n, i + k)} * ((${numerator} ^ (${n} - ${i + k})) / (${denominator} ^ ${n}))`)
           }
           return result
         } catch (error) { console.warn(error) }
       }
     },
     filters: {
-      percentage: function (value) {
+      percentage (value) {
         try {
           return math.format(math.eval(`${value} * 100`), {notation: 'fixed', precision: 2}) + ' %'
         } catch (error) { console.warn(error) }
